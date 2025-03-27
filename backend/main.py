fdcfd0e6bf274b13846ba938c5cd08721a9d0810
André Sousa <andrereissousadev@gmail.com>
2025-03-27 21:48:29 +0000

    Added base endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,37 @@
+from typing import List
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class LabelInput(BaseModel):
+    labels: List[str]
+    
+class SelectedModel(BaseModel):
+    model: str
+
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "API para gerar descrições a partir de labels."}
+
+
+@app.get("/image/models")
+def get_image_models():
+    return ["Modelo 1", "Modelo 2", "Modelo 1 + 2"]
+
+
+@app.get("/description/models")
+def get_description_models():
+    return ["Custom", "Pre-built"]
+
+
+@app.post("/image/findLabels")
+def find_labels_from_image():
+    return "Not implemented yet"
+
+
+@app.post("/description/generate")
+def generate_description(model: SelectedModel, data: LabelInput):
+    return "Not implemented yet"
